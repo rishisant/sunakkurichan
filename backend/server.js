e3e4d9d2b44e81355353d3de0f2ea286fd5d0536
taishinanbo <namba@sainta.co.jp>
2025-07-01 16:05:40 +0900

    【追加】envの追加

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB接続成功!"))
+  .catch((err) => console.error("❌ MongoDB 接続失敗:", err));
+
+// Expressアプリケーションの初期化
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,3 +24,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`サーバー起動: http://localhost:${PORT}`));
+
