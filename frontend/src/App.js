a450e354d669adb8ed9c9ba694dc5d34a510671b
Rishi Santhanam <rishisant@tamu.edu>
2025-07-01 16:06:44 +0900

    【追加】画像、基本トースト機能、BrowserRouterを実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Zoom } from 'react-toastify';
+
 function App() {
   return (
     <div className="App">
-      
+      <ToastContainer
+          position="top-right"
+          autoClose={2500}
+          hideProgressBar={true}
+          closeOnClick
+          pauseOnHover
+          draggable
+          toastClassName="toast-base"
+          bodyClassName="toast-body"
+          progressClassName="toast-progress"
+          transition={Zoom}
+          transitionDuration={500}
+        />
+
     </div>
   );
 }
